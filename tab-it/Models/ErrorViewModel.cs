5b886ac339acde99b4daea712345598a01e28fb5
MatijaTrstenjak <matajaj567@gmail.com>
2026-03-30 22:03:23 +0200

    initial project code

---
+++ tab-it/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tab_it.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
